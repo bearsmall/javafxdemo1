14bf0a16ee72980539bf5732ab7394b2ff28f558
bearsmall <xy196456>
2018-11-24 21:42:02 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaFX demo
+
+##tutorial 
+> https://code.makery.ch/zh-cn/library/javafx-tutorial/
---
+++ src/com/xy/adapter/LocalDateAdapter.java
@@ -0,0 +1,24 @@
+package com.xy.adapter;
+
+import java.time.LocalDate;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * Adapter (for JAXB) to convert between the LocalDate and the ISO 8601 
+ * String representation of the date such as '2012-12-03'.
+ * 
+ * @author Marco Jakob
+ */
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+    @Override
+    public LocalDate unmarshal(String v) {
+        return LocalDate.parse(v);
+    }
+
+    @Override
+    public String marshal(LocalDate v) {
+        return v.toString();
+    }
+}
